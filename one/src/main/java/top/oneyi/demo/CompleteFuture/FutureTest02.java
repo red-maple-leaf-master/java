717c0c0d46a96243063559d24b9a740e48f71571
伊万 <1362187574@qq.com>
2023-02-06 17:05:42 +0800

    如何创建CompletableFuture对象以及常用的回调方法

---
+++ one/src/main/java/top/oneyi/demo/CompleteFuture/FutureTest02.java
@@ -0,0 +1,44 @@
+package top.oneyi.demo.CompleteFuture;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+/**
+ * 使用CompletableFuture 实现异步调用
+ * @author oneyi
+ * @date 2023/2/6
+ */
+
+public class FutureTest02 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        long start = System.currentTimeMillis();
+        CompletableFuture<String> uCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return "成功获取客户信息";
+        });
+
+        Thread.sleep(200);
+        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(new Supplier<String>() {
+
+            @SneakyThrows
+            @Override
+            public String get() {
+                Thread.sleep(200);
+                return "成功获取客户订单信息";
+            }
+        });
+        String mdCus = uCompletableFuture.get();
+        System.out.println("mdCus = " + mdCus);
+        String mdOrder = stringCompletableFuture.get();
+        System.out.println("mdOrder = " + mdOrder);
+        long end = System.currentTimeMillis();
+        System.out.println("一共耗时" +  (end - start) );
+    }
+}
---
+++ one/src/main/java/top/oneyi/demo/CompleteFuture/FutureTest03.java
@@ -0,0 +1,49 @@
+package top.oneyi.demo.CompleteFuture;
+
+import java.util.concurrent.CompletableFuture;
+/**
+ *  thenApply
+ * @author oneyi
+ * @date 2023/2/6
+ */
+
+public class FutureTest03 {
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("我是异步异步实行的第一步  runAsync 我没有返回值");
+        });
+
+        Thread.sleep(200);
+        CompletableFuture<String> uCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("我是异步实行的第二步  supplyAsync  我有返回值");
+            return "执行成功";
+        });
+        // 接收前置任务的返回值进行 处理并返回  238  需要等前置处理之后才能接着进行下面的操作
+ /*       CompletableFuture<String> objectCompletableFuture = uCompletableFuture.thenApply((s) -> {
+            System.out.println(s);
+            return "我是中间处理流程处理的信息为" + s ;
+        });*/
+ // 259   异步  是另一个新的线程进行的操作,不在是原来的线程
+        CompletableFuture<String> objectCompletableFuture = uCompletableFuture.thenApplyAsync((s) -> {
+            System.out.println(s);
+            return "我是中间处理流程处理的信息为" + s ;
+        });
+        long end = System.currentTimeMillis();
+
+        System.out.println("voidCompletableFuture.join() = " + voidCompletableFuture.join());
+        System.out.println("uCompletableFuture.join() = " + uCompletableFuture.join());
+        System.out.println("objectCompletableFuture.join() = " + objectCompletableFuture.join());
+        System.out.println("一共耗时为: "+(end - start));
+    }
+}
