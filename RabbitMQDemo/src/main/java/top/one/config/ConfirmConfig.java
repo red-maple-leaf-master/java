ffef02dd3e3fc2df210ebf69fd78486b52725250
伊万 <1362187574@qq.com>
2023-02-17 14:57:25 +0800

    延时队列实现

--- RabbitMQDemo/src/main/java/top/one/config/ConfirmConfig.java
+++ RabbitMQDemo/src/main/java/top/one/config/ConfirmConfig.java
@@ -16,51 +16,57 @@ public class ConfirmConfig {
 
     // 声明确认队列
     @Bean("confirmQueue")
-    public Queue confirmQueue(){
+    public Queue confirmQueue() {
         return QueueBuilder.durable(CONFIRM_QUEUE_NAME).build();
     }
+
     //声明确认队列绑定关系
     @Bean
     public Binding queueBinding(@Qualifier("confirmQueue") Queue queue,
-                                @Qualifier("confirmExchange") DirectExchange exchange){
+                                @Qualifier("confirmExchange") DirectExchange exchange) {
         return BindingBuilder.bind(queue).to(exchange).with("key1");
     }
+
     //声明备份 Exchange
     @Bean("backupExchange")
-    public FanoutExchange backupExchange(){
+    public FanoutExchange backupExchange() {
         return new FanoutExchange(BACKUP_EXCHANGE_NAME);
     }
+
     //声明确认 Exchange 交换机的备份交换机
     @Bean("confirmExchange")
-    public DirectExchange confirmExchange(){
+    public DirectExchange confirmExchange() {
         ExchangeBuilder exchangeBuilder =
                 ExchangeBuilder.directExchange(CONFIRM_EXCHANGE_NAME)
                         .durable(true)
-//设置该交换机的备份交换机
+                        //设置该交换机的备份交换机
                         .withArgument("alternate-exchange", BACKUP_EXCHANGE_NAME);
-        return (DirectExchange)exchangeBuilder.build();
+        return (DirectExchange) exchangeBuilder.build();
     }
+
     // 声明警告队列
     @Bean("warningQueue")
-    public Queue warningQueue(){
+    public Queue warningQueue() {
         return QueueBuilder.durable(WARNING_QUEUE_NAME).build();
     }
+
     // 声明报警队列绑定关系
     @Bean
     public Binding warningBinding(@Qualifier("warningQueue") Queue queue,
-                                  @Qualifier("backupExchange") FanoutExchange
-                                          backupExchange){
+                                  @Qualifier("backupExchange") FanoutExchange backupExchange) {
         return BindingBuilder.bind(queue).to(backupExchange);
     }
+
     // 声明备份队列
     @Bean("backQueue")
-    public Queue backQueue(){
+    public Queue backQueue() {
         return QueueBuilder.durable(BACKUP_QUEUE_NAME).build();
     }
+
     // 声明备份队列绑定关系
     @Bean
     public Binding backupBinding(@Qualifier("backQueue") Queue queue,
-                                 @Qualifier("backupExchange") FanoutExchange backupExchange){
+                                 @Qualifier("backupExchange") FanoutExchange backupExchange) {
         return BindingBuilder.bind(queue).to(backupExchange);
     }
 }
