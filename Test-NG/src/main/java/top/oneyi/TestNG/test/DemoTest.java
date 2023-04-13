35339e13c4160035f23c4a666cb1a3cbab00ea47
伊万 <1362187574@qq.com>
2023-04-13 17:04:27 +0800

    TestNG 的简单应用

---
+++ Test-NG/src/main/java/top/oneyi/TestNG/test/DemoTest.java
@@ -0,0 +1,12 @@
+package top.oneyi.TestNG.test;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+
+    @Test
+    public void test(){
+        System.out.println("我是业务方法");
+    }
+}
