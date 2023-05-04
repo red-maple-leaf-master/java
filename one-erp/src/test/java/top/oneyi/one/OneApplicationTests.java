8e819078f1376213bd2f357a10c56b503e0875ba
伊万 <1362187574@qq.com>
2023-05-04 14:24:26 +0800

    no commit message

---
+++ one-erp/src/test/java/top/oneyi/one/OneApplicationTests.java
@@ -0,0 +1,14 @@
+package top.oneyi.one;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
