8ba1c085dec9acd02b4b42d7fc3e784731ce33db
伊万 <1362187574@qq.com>
2023-05-06 16:35:50 +0800

    no commit message

--- mybatis-plus-demo/src/main/java/top/oneyi/mapper/UserMapper.java
+++ mybatis-plus-demo/src/main/java/top/oneyi/mapper/UserMapper.java
@@ -3,7 +3,6 @@ package top.oneyi.mapper;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Mapper;
 import top.oneyi.pojo.User;
 
