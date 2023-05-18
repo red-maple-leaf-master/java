9b3ba0bcac5ae1a9f1e93908be9403898d2db962
伊万 <1362187574@qq.com>
2023-05-18 17:00:16 +0800

    changes

--- API_doc_2/src/main/java/top/oneyi/api/SwaggerDemoApplication.java
+++ API_doc_2/src/main/java/top/oneyi/api/SwaggerDemoApplication.java
@@ -2,11 +2,11 @@ package top.oneyi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 // 启用swagger
-@EnableSwagger2
+//@EnableSwagger2
 public class SwaggerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerDemoApplication.class, args);
