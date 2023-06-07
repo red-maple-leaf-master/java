257b30653f21db86a48ef47efd9b8b61a90d1e1f
伊万 <1362187574@qq.com>
2023-06-07 18:15:27 +0800

    no commit message

--- API_doc/src/test/java/top/oneyi/api/wordTest.java
+++ API_doc/src/test/java/top/oneyi/api/wordTest.java
@@ -1,16 +1,33 @@
 package top.oneyi.api;
 
-import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import com.deepoove.poi.XWPFTemplate;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import top.oneyi.api.entity.Data;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+import java.util.HashMap;
 
-//@SpringBootTest//加载 SpringBoot 启动类
+@SpringBootTest//加载 SpringBoot 启动类
 public class wordTest {
 
 
+    @Test
     public void test() throws Exception {
-
+        Data data = new Data();
+        data.setName("张三");
+        data.setTitle("入学通知书");
+        data.setStartTime(new Date().toString());
+        XWPFTemplate template = XWPFTemplate.compile("template.docx").render(
+//                new HashMap<String, Object>(){{
+//                    put("title", "Hi, poi-tl Word模板引擎");
+//                }}
+                data
+                );
+        template.writeAndClose(new FileOutputStream("output.docx"));
     }
 
 
