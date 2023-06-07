4fd4eeac21636c5eac46c8d820b43c1bb5e029cb
伊万 <1362187574@qq.com>
2023-06-07 22:46:43 +0800

    no commit message

--- one/src/main/java/top/oneyi/controller/TestController.java
+++ one/src/main/java/top/oneyi/controller/TestController.java
@@ -4,14 +4,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
 
     @GetMapping
-    public String test(){
+    public String test(HttpServletResponse response) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream("C:\\Users\\13621\\Desktop\\test.docx");
+
+        response.addHeader("Content-Disposition",
+                "attachment;filename=" + new String("out.docx".getBytes(StandardCharsets.UTF_8),
+                        StandardCharsets.ISO_8859_1));
+        ServletOutputStream outputStream = response.getOutputStream();
+        int len=0;
+        while((len=fileInputStream.read())!=-1){
+            outputStream.write(len);
+        }
         System.out.println("controller被调用");
         return "成功 调用";
     }
+
 }
