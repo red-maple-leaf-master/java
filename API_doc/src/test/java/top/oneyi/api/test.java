9fc868edc7f51cd001978f45888101343b43c24c
伊万 <1362187574@qq.com>
2023-06-09 15:07:11 +0800

    no commit message

---
+++ API_doc/src/test/java/top/oneyi/api/test.java
@@ -0,0 +1,34 @@
+package top.oneyi.api;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+public class test {
+
+    @Test
+    public void test01(){
+        String file = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\temp.html";
+        // 获取HTML文件流
+        StringBuffer htmlSb = new StringBuffer();
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(file), "utf-8"));
+            while (br.ready()) {
+                htmlSb.append(br.readLine());
+            }
+            br.close();
+            // 删除临时文件
+            //file.delete();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // HTML文件字符串
+        String htmlStr = htmlSb.toString();
+        // 返回经过清洁的html文本
+        System.out.println( htmlStr);
+    }
+}
--- API_doc/src/test/java/top/oneyi/api/wordTest.java
+++ API_doc/src/test/java/top/oneyi/api/wordTest.java
@@ -1,19 +1,29 @@
 package top.oneyi.api;
 
 
+//import fr.opensagres.odfdom.converter.core.BasicURIResolver;
 
 
-import com.documents4j.api.DocumentType;
-import com.documents4j.api.IConverter;
-import com.documents4j.job.LocalConverter;
-import org.apache.tomcat.util.http.ResponseUtil;
+//import fr.opensagres.odfdom.converter.xhtml.XHTMLConverter;
+
+import fr.opensagres.poi.xwpf.converter.core.BasicURIResolver;
+import fr.opensagres.poi.xwpf.converter.core.FileImageExtractor;
+import fr.opensagres.poi.xwpf.converter.core.FileURIResolver;
+import fr.opensagres.poi.xwpf.converter.xhtml.XHTMLConverter;
+import fr.opensagres.poi.xwpf.converter.xhtml.XHTMLOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import top.oneyi.api.common.utils.PdfUtil;
-import top.oneyi.api.entity.Data;
+import top.oneyi.api.common.utils.WordToHtmlUtil;
 
+
+import javax.lang.model.element.Element;
+import javax.lang.model.util.Elements;
 import java.io.*;
 import java.net.URL;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -22,40 +32,39 @@ public class wordTest {
 
 
     @Test
-    public void test01(){
-        String wordPath = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\output.docx";
-        String pdfPath = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\www.pdf";
+    public void test01() throws IOException {
 
-        PdfUtil.docxToPdf(wordPath,pdfPath);
 
-        File inputFile = new File(wordPath);
-        File outputFile = new File(pdfPath);
+        //存放图片的目的地址（）
+        String imagePath = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\";
+        //需要被转换的docx文件
+        String sourceFileName = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\temp.docx";
+        //转换成的html文件(不存在将会被创建，存在会被覆盖)
+        String targetFileName = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\temp.html";
+        OutputStreamWriter outputStreamWriter = null;
         try {
-            InputStream inputStream = new FileInputStream(inputFile);
-            OutputStream outputStream = new FileOutputStream(outputFile);
+            XWPFDocument document = new XWPFDocument(new FileInputStream(sourceFileName));
+            XHTMLOptions options = XHTMLOptions.create();
+            options.setIgnoreStylesIfUnused(false);
+            options.setFragment(true);
 
-
-            IConverter converter = LocalConverter.builder().build();
-            converter.convert(inputStream).as(DocumentType.DOCX).to(outputStream).as(DocumentType.PDF).execute();
-
-
-
-
-            ByteArrayInputStream parse = parse(outputStream);
-            OutputStream outputStream01 = new FileOutputStream("E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\www001.pdf");
-            converter.convert(parse).as(DocumentType.DOCX).to(outputStream01).as(DocumentType.PDF).execute();
-            outputStream.close();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            // 存放图片的文件夹
+            options.setExtractor(new FileImageExtractor(new File(imagePath)));
+            // html中图片的路径
+            options.URIResolver(new BasicURIResolver("image"));
+            outputStreamWriter = new OutputStreamWriter(new FileOutputStream(targetFileName), "utf-8");
+            XHTMLConverter xhtmlConverter = (XHTMLConverter) XHTMLConverter.getInstance();
+            xhtmlConverter.convert(document, outputStreamWriter, options);
+        } finally {
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
         }
 
-        /*
-        Convert2Pdf.doc2Pdf(wordPath,pdfPath);
-        System.out.println("转换成功");
-        File file = new File(pdfPath);
-        ResponseUtil.onlineReader(file,response);*/
+
     }
+
+
     // outputStream转inputStream
     public static ByteArrayInputStream parse(OutputStream out) throws Exception {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -65,25 +74,62 @@ public class wordTest {
     }
 
     @Test
-    public void test02(){
-        //1.调用类对象的getResource方法获取文件
-        URL resource = Test.class.getResource("");
-        System.out.println(resource);
+    public void test02() throws Exception {
 
-        URL ifFileNotExist = Test.class.getResource("test.txt");
-        System.out.println(resource);
 
-        //2.调用类加载器的getResource方法获取文件
-        resource = Test.class.getClassLoader().getResource("test.txt");
-        System.out.println(resource);
+   /*     docx("E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\",
+               "temp.docx","temp01.html" );*/
 
-        //3.调用类加载器的getResource方法获取文件，并调用返回的url对象的getPath获取路径
-        String path = Objects.requireNonNull(Test.class.getClassLoader().getResource("test.txt")).getPath();
-        System.out.println(path);
 
-        //4.调用系统变量的getProperty方法获取user.dir路径
-        String property = System.getProperty("user.dir");
-        System.out.println(property);
+        WordToHtmlUtil.docx("E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\",
+                "temp.docx", "temp02.html");
+    }
+
+
+    // https://blog.csdn.net/yuxielea/article/details/103477874
+
+    /**
+     * @Author: jimmy
+     * @Date: 2019/12/10 12:34
+     * @Param: [filePath, fileName, htmlName]
+     * @Return: void
+     * @Description: 转换docx
+     */
+    public static void docx(String filePath, String fileName, String htmlName) throws Exception {
+        final String filePathName = filePath + fileName;
+        InputStream in = new FileInputStream(new File(filePathName));
+        // 1.加载解析docx文档用的XWPFDocument对象
+        XWPFDocument document = new XWPFDocument(in);
+        // 2.解析XHTML配置 (这里设置IURIResolver来设置图片存放的目录)
+        File imageFolderFile = new File(filePath);
+        XHTMLOptions options = XHTMLOptions.create().URIResolver(new FileURIResolver(imageFolderFile));
+        options.setExtractor(new FileImageExtractor(imageFolderFile));
+        options.setIgnoreStylesIfUnused(false);
+        options.setFragment(true);
+        // 3.将 XWPFDocument转换成XHTML
+        OutputStream out = new FileOutputStream(new File(filePath + htmlName));
+        XHTMLConverter.getInstance().convert(document, out, options);
+    }
+
+
+    /**
+     * @Author: jimmy
+     * @Date: 2019/12/10 14:54
+     * @Param: []
+     * @Return: void
+     * @Description: word转换html并生成html文件
+     */
+    @Test
+    void WordToHtmlTest() throws Exception {
+
+        String filePath = "E:\\Desktop\\java_project\\one\\java\\API_doc\\src\\main\\resources\\template\\";
+        String singleFileName = "ww";
+        //遍历所有文件
+
+        //生成html
+
+        docx(filePath, "temp.docx", singleFileName + ".html");
+
 
     }
 
