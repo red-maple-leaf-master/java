cc61d35673a24711a7c0964a167cbbb27a1715af
伊万 <1362187574@qq.com>
2023-06-18 15:25:19 +0800

    no commit message

---
+++ freemarkerToPDF/src/main/java/com/example/demo/pojo/ContractParticularsVo.java
@@ -0,0 +1,117 @@
+package com.example.demo.pojo;
+
+
+
+import lombok.Data;
+
+/**
+ * 合同详情
+ *
+ * @author oneyi
+ * @date 2023/6/6
+ */
+
+@Data
+public class ContractParticularsVo   {
+    /**
+     *
+     */
+
+    private Long id;
+    /**
+     * 贷款编码
+     */
+    private String loanCode;
+    /**
+     * 企业编码
+     */
+    private String enterpriseCode;
+
+    /**
+     * 企业名称
+     */
+    private String enterpriseName;
+    /**
+     * 产品名称
+     */
+    private String productName;
+
+    /**
+     * 借款金额
+     */
+    private String loanAmount;
+    /**
+     * 大写借款金额
+     */
+    private String capitalLoanAmount;
+    /**
+     * 期限类型
+     */
+    private String termType;
+    /**
+     * 期限
+     */
+    private String timeLimit;
+    /**
+     * 年利率
+     */
+    private String annualInterestRate;
+    /**
+     * 月利率
+     */
+    private String monthInterestRate;
+    /**
+     * 还款方式
+     */
+    private String repaymentMode;
+
+    /**
+     * 违约利率
+     */
+    private String violateInterestRate;
+    /**
+     * 手续费率
+     */
+    private String procedureInterestRate;
+    /**
+     * 甲方银行户名
+     */
+    private String partyBankAccountName;
+    /**
+     * 甲方银行账号
+     */
+    private String partyBankAccount;
+    /**
+     * 开户行
+     */
+    private String openingBank;
+    /**
+     * 状态  0 未签订 1 已签订
+     */
+    private String status;
+    /**
+     * 合同地址
+     */
+    private String contractImageUrl;
+    /**
+     * 客户
+     */
+    private EnterpriseBaseInfo enterpriseBaseInfo;
+    /**
+     * 借款主体
+     */
+    private String loanSubject;
+
+    /**
+     * 借款用途
+     */
+    private String loanPurpose;
+    /**
+     * 社会信用代码
+     */
+    private String creditCode;
+    /**
+     * 工商注册资金
+     */
+    private String registeredCapital;
+}
---
+++ freemarkerToPDF/src/main/java/com/example/demo/pojo/EnterpriseInfo.java
@@ -0,0 +1,83 @@
+package com.example.demo.pojo;
+
+
+import lombok.Data;
+
+import java.util.Date;
+
+
+/**
+ * 【请填写功能名称】对象 enterprise_info
+ *
+ * @author ruoyi
+ * @date 2023-06-06
+ */
+@Data
+public class EnterpriseInfo {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     *
+     */
+    private Long id;
+    /**
+     * 企业名称
+     */
+    private String enterpriseName;
+    /**
+     * 企业法人
+     */
+    private String enterpriseLegalPerson;
+    /**
+     * 委托代理人
+     */
+    private String entrustedAgent;
+    /**
+     * 委托代理人联系方式
+     */
+    private String entrustedContact;
+    /**
+     * 企业成立时间
+     */
+    private Date establishedTime;
+    /**
+     * 纳税人识别号
+     */
+    private String taxpayerNum;
+    /**
+     * 开户银行
+     */
+    private String depositBank;
+    /**
+     * 银行账户
+     */
+    private String bankAccount;
+    /**
+     * 邮寄地址
+     */
+    private String mailingAddress;
+    /**
+     * 接收人
+     */
+    private String receiver;
+    /**
+     * 接收人联系方式
+     */
+    private String receiverContact;
+
+    /*营业执照*/
+    private String businessLicense;
+
+    /*法人身份证*/
+    private String identityCard;
+
+    /*住所地*/
+    private String domicile;
+
+    /*乙方联系方式*/
+    private String contact;
+
+    /*银行户名*/
+    private String accountName;
+}
