6a6e8b7e3949a14e6aed9e7dca8180608cf3c094
伊万 <1362187574@qq.com>
2023-07-04 09:49:53 +0800

    init

---
+++ mess/open_feign/client/src/main/java/top/oneyi/serverApi/TestApi.java
@@ -0,0 +1,16 @@
+package top.oneyi.serverApi;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Component
+@FeignClient(name = "one-server",url = "localhost:1011")
+//@FeignClient(name = "one-server")
+public interface TestApi {
+    @GetMapping("/test/one")
+    String list(@RequestParam("str") String str);
+    @GetMapping("/test01/one")
+    String list01(@RequestParam("i") Integer i);
+}
---
+++ mess/open_feign/server/src/main/java/top/oneyi/ServiceApplication.java
@@ -0,0 +1,14 @@
+package top.oneyi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class ServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+}
