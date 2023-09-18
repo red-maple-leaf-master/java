81ba090663fa46cc371671d456cf1dba367b0411
伊万 <1362187574@qq.com>
2023-09-18 11:43:03 +0800

    no commit message

--- mess/one/src/test/java/top/oneyi/leetCode.java
+++ mess/one/src/test/java/top/oneyi/leetCode.java
@@ -3,10 +3,8 @@ package top.oneyi;
 import io.swagger.models.auth.In;
 import org.junit.Test;
 
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.Queue;
+import javax.swing.tree.TreeNode;
+import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
 
@@ -342,6 +340,37 @@ class CQueue{
         while(!inStack.isEmpty()){
             outStack.push(inStack.pop());
         }
+    }
+
+   class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+     }
+  }
+
+    Map<TreeNode, Integer> f = new HashMap<TreeNode, Integer>();
+    Map<TreeNode, Integer> g = new HashMap<TreeNode, Integer>();
+
+    public int rob(TreeNode root) {
+        dfs(root);
+        return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+    }
+
+    public void dfs(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        dfs(node.left);
+        dfs(node.right);
+        f.put(node, node.val + g.getOrDefault(node.left, 0) + g.getOrDefault(node.right, 0));
+        g.put(node, Math.max(f.getOrDefault(node.left, 0), g.getOrDefault(node.left, 0)) + Math.max(f.getOrDefault(node.right, 0), g.getOrDefault(node.right, 0)));
     }
 
 
