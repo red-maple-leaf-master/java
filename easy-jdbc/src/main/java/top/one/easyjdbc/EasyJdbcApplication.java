9773877f3ba9e1e18f2fe726969ecd4ad0df4872
伊万 <1362187574@qq.com>
2023-09-22 17:37:49 +0800

    创建easy-jdbc

---
+++ easy-jdbc/src/main/java/top/one/easyjdbc/EasyJdbcApplication.java
@@ -0,0 +1,13 @@
+package top.one.easyjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyJdbcApplication.class, args);
+    }
+
+}
---
+++ easy-jdbc/src/test/java/top/one/easyjdbc/EasyJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package top.one.easyjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
