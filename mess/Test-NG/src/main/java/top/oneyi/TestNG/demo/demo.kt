f201969e17f1e8d6b6202529ea59e540680100fa
伊万 <1362187574@qq.com>
2023-11-20 18:14:26 +0800

    okhttp3的简单使用

---
+++ mess/Test-NG/src/main/java/top/oneyi/TestNG/demo/demo.kt
@@ -0,0 +1,15 @@
+package top.oneyi.TestNG.demo
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: wanyi
+ * @Date: 2023/11/20/17:57
+ */
+fun main() {
+    println("Hello, world!")
+    // Hello, world!
+
+    var customer = 10;
+    println("There are $customer customer");
+}
