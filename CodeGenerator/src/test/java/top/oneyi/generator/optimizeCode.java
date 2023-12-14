14b2e3f12f0b29a681c8c0d91b37683d019c50b3
伊万 <1362187574@qq.com>
2023-12-14 09:21:30 +0800

    1, mysql插入一百万条数据,耗时24s(ai优化过,添加了异步操作,添加了线程池) 2,直接手动创建10条线程进行操作,耗时 8s

--- CodeGenerator/src/test/java/top/oneyi/generator/optimizeCode.java
+++ CodeGenerator/src/test/java/top/oneyi/generator/optimizeCode.java
@@ -1,43 +1,33 @@
 package top.oneyi.generator;
 
-import com.alibaba.druid.sql.ast.statement.SQLForeignKeyImpl;
-import lombok.SneakyThrows;
+
 import lombok.extern.java.Log;
 import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StopWatch;
-import org.springframework.web.client.RestTemplate;
-
-
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 
 /**
  * Created with IntelliJ IDEA.
+ * 经过AI优化过的代码
  *
  * @Author: wanyi
- * @Date: 2023/09/19/17:56
+ * @Date: 2023/12/14/9:14
  */
-
 @Log
 @SpringBootTest
-public class test {
-
-
-
+public class optimizeCode {
     @Value("${spring.datasource.url}")
     private String url;
     @Value("${spring.datasource.username}")
@@ -58,56 +48,28 @@ public class test {
         log.info("生成用户记录总数: "+ userList.size());
         // 将list分成十份
         int chunkSize = userList.size() / 10;
-        //Thread[] threads = new Thread[10];
-        // 创建线程池
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
         stopWatch.start();
-
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        // jdk自带的异步类
+        CompletableFuture<?>[] futures = new CompletableFuture<?>[10];
         for (int i = 0; i < 10; i++) {
-
             List<User> chunk = userList.subList(i * chunkSize, (i +1 ) * chunkSize);
-          /*  threads[i] = new Thread(() -> {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
                 try {
                     extracted(chunk);
                 } catch (SQLException throwable) {
                     throwable.printStackTrace();
                 }
-            });
-*/
-            /*log.info("开启线程: "+ threads[i].getName());
-            threads[i].start();*/
-
-            Runnable worker = () -> {
-                try {
-                    boolean success = extracted(chunk);
-                    if (!success) {
-                        throw new RuntimeException("数据插入失败");
-                    }
-                } catch (SQLException throwable) {
-                    throwable.printStackTrace();
-                }
-            };
-            // 将线程放到线程池
-            executorService.execute(worker);
-
+            }, executorService);
+            futures[i] = future;
         }
-        // 等待线程执行完毕
-      /*  for (Thread thread : threads) {
-            thread.join();
-        }*/
-        // extracted(stopWatch, userList);
 
-        executorService.shutdown();
-        while (!executorService.isTerminated()) {}
+        CompletableFuture.allOf(futures).join();
+
         stopWatch.stop();
         System.out.println("使用原生jdbc插入100万条数据总耗时:  "+stopWatch.getTotalTimeMillis() + " ms");
     }
 
-    /**
-     * 向数据库插入数据
-     * @param userList 需要插入的用户集合
-     * @throws SQLException 抛出的异常
-     */
     @Transactional
     public boolean extracted(List<User> userList) throws SQLException {
         String sql = "insert into user(nick_name,age) values (?,?)";
@@ -126,7 +88,6 @@ public class test {
         return true;
     }
 
-
     private List<User> generateUser(int count){
         List<User> list = new ArrayList<>();
         for (int i = 0; i < count; i++) {
@@ -137,20 +98,5 @@ public class test {
         }
 
         return list;
-    }
-
-    @Test
-    public void BigData1000wTest() throws Exception {
-        //   通过JDBCUtil工具类获取数据库连接对象
-        //Connection conn = BaseDao.getConn("million-test", "root", "root");
-        //   StreamUtil是已经封装好的使用流读取文件的工具类
-        //List<String> list = StreamUtil.readingLineFormTextFile(new File("D://milliondatatest//test(500W).csv"));
-        String sql = "insert into mysqltest values(?,?,?,?)";   //  定义要导入数据的sql,无需主键将第一个?设置为null
-        long start = System.currentTimeMillis();   //   获取方法开始执行前的时间（单位：毫秒）
-        //  调用刚刚封装好的工具类
-        //DataImport.dispose(conn, list, 0, true, 1000000, sql);
-        long end = System.currentTimeMillis();     //   获取方法执行结束后的时间
-        //   相减即可得到插入所有数据的耗时   秒=毫秒/1000;
-       // System.out.println("成功导入" + list.size() + "条数据！！时长：" + (end - start) / 1000 + "秒");
     }
 }
